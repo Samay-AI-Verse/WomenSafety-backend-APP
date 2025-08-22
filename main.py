227fafe953d673d862bbf734e10c47a2bddbfc26
Samay <samaypowade9@gmail.com>
2025-08-22 10:22:40 +0530

    updated redirect URL

--- main.py
+++ main.py
@@ -13,7 +13,8 @@ load_dotenv()
 # ----------------------------
 GOOGLE_CLIENT_ID = os.getenv("GOOGLE_CLIENT_ID", "<YOUR_GOOGLE_CLIENT_ID>")
 GOOGLE_CLIENT_SECRET = os.getenv("GOOGLE_CLIENT_SECRET", "<YOUR_GOOGLE_CLIENT_SECRET>")
-REDIRECT_URI = "http://localhost:8000/auth/callback"
+REDIRECT_URI = "https://womensafety-backend-app.onrender.com/auth/callback"
+
 
 SECRET_KEY = os.getenv("SECRET_KEY", "supersecret")
 MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017/")
